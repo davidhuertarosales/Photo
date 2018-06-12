29ec40f9680d2350df5ba5f1e043df93d8bb095d
david huerta <quemonito19@hotmail.com>
2018-06-12 00:19:55 -0500

    primero

---
+++ PhotoSharingApplication/Models/PhotoSharingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingContext : System.Data.Entity.DbContext
+    {
+        public PhotoSharingContext() : base()
+        {
+            this.Database.CommandTimeout = 180;
+        }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
